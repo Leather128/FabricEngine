7637539b5f245ce5a9386adf74a65952c6502958
Leather128 <81660872+Leather128@users.noreply.github.com>
2022-10-29 01:29:22 -0400

    flixel fork is now https://github.com/Leather128/flixel.git

--- docs/docs.sh
+++ docs/docs.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 haxe docs/docs.hxml
-haxelib run dox -i ../ -o ../pages --title "Fabric Engine Documentation" -ex .*^ -in base/* -in external/* -in funkin/*
+haxelib run dox -i docs -o pages --title "Fabric Engine Documentation" -ex .*^ -in base/* -in external/* -in funkin/*
