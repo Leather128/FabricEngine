Leather128/FabricEngine

commit 587f2610eb4841fcf040e589415c74e5bd77c5b7

you can now assign `note_input` to a custom function to create custom input for mods

src/funkin/scenes/Gameplay.hx
@@ -111,6 +111,13 @@ class Gameplay extends FunkinScene {
      */
     public var started_countdown:Bool = false;
 
+    /**
+     * Field for the `haxe.Constraints.Function` in `ui.note_input`.
+     * 
+     * (Used for scripting)
+     */
+    public var note_input(default, set):haxe.Constraints.Function;
+
     private function set_camera_speed(value:Float):Float {
         FlxG.camera.followLerp = value / 100.0; camera_speed = value;
         return value;
@@ -118,6 +125,11 @@ class Gameplay extends FunkinScene {
 
     private function set_camera_moving(value:Bool):Bool {
         FlxG.camera.followActive = value; camera_moving = value;
+        return value;
+    }
+
+    private function set_note_input(value:haxe.Constraints.Function):haxe.Constraints.Function {
+        if (ui != null) ui.note_input = function():Void { Reflect.callMethod(null, value, [ ui ]); };
         return value;
     }
 
@@ -135,6 +147,11 @@ class Gameplay extends FunkinScene {
         FlxG.sound.music.stop();
 
         super.create();
+
+        // load ui
+        ui = new funkin.sprites.ui.GameplayUI(); ui.scrollFactor.set();
+        ui.cameras = [hud_cam];
+        note_input = ui.note_input;
 
         instance = this;
 
@@ -165,9 +182,7 @@ class Gameplay extends FunkinScene {
         // for tutorial type shit
         if (dad.character == gf.character) { scripts.remove(dad.script); remove(dad); dad.destroy(); dad = gf; }
 
-        // load ui
-        ui = new funkin.sprites.ui.GameplayUI(); ui.scrollFactor.set();
-        ui.cameras = [hud_cam];
+        // add ui to screen
         add(ui);
 
         // load music

src/funkin/sprites/ui/GameplayUI.hx
@@ -204,7 +204,7 @@ class GameplayUI extends flixel.group.FlxSpriteGroup {
 	/**
 	 * Checks your inputs and reacts appropriately.
 	 */
-	public function note_input():Void {
+	public dynamic function note_input():Void {
 		var note_states:Array<flixel.input.FlxInput.FlxInputState> = [Input.get('keys-4-0'), Input.get('keys-4-1'), Input.get('keys-4-2'), Input.get('keys-4-3')];
 
 		// TODO: add sustain note hitting

commit 6052fab80a0778c218cf0d9c2db19463c32d545c

fix some bugs regarding caused by 587f2610eb4841fcf040e589415c74e5bd77c5b7

src/funkin/scenes/Gameplay.hx
@@ -149,10 +149,6 @@ class Gameplay extends FunkinScene {
         super.create();
 
         // load ui
-        ui = new funkin.sprites.ui.GameplayUI(); ui.scrollFactor.set();
-        ui.cameras = [hud_cam];
-        note_input = ui.note_input;
-
         instance = this;
 
         // song script inits
@@ -183,6 +179,11 @@ class Gameplay extends FunkinScene {
         if (dad.character == gf.character) { scripts.remove(dad.script); remove(dad); dad.destroy(); dad = gf; }
 
         // add ui to screen
+        ui = new funkin.sprites.ui.GameplayUI(); ui.scrollFactor.set();
+        ui.cameras = [hud_cam];
+        if (note_input == null) note_input = ui.note_input;
+        else note_input = note_input; // this sets ui.note_input to note_input
+
         add(ui);
 
         // load music